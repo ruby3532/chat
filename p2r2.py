f793b59c0265773da4ca3892b1025c4303c43ad5
李心汝 <lixinru@lixinrudeAir.Home>
2021-12-19 20:41:10 +0800

    計算對話記錄中的字；貼圖；圖

---
+++ p2r2.py
@@ -0,0 +1,53 @@
+
+# 讀取檔案
+def read_file(filename):
+	lines = []
+	with open(filename, 'r', encoding = 'utf-8-sig') as f:
+		for line in f:
+			lines.append(line.strip())
+	return lines
+
+# 計算說的話和貼圖、圖片
+def count(lines):
+	person = None # 無的預設值
+	allen_word_count = 0
+	allen_sticker_count = 0
+	allen_image_count = 0
+	viki_word_count = 0
+	viki_sticker_count = 0
+	viki_image_count = 0
+	for line in lines:
+		s = line.split(' ')
+		time = s[0]
+		name = s[1]
+		if name == 'Allen':
+			if s[2] == '貼圖':
+				allen_sticker_count += 1
+			elif s[2] == '圖片':
+				allen_image_count += 1
+			else:
+				for m in s[2:]:
+					allen_word_count +=	len(m)
+		elif name == 'Viki':
+			if s[2] == '貼圖':
+				viki_sticker_count += 1
+			elif s[2] == '圖片':
+				viki_image_count += 1
+			else:
+				for m in s[2:]:
+					viki_word_count += len(m)
+
+	print('allen 說了' , allen_word_count, '。', '傳了', allen_sticker_count, '個貼圖', '以及', allen_image_count, '張圖片')
+	print('viki 說了' , viki_word_count, '。', '傳了', viki_sticker_count, '個貼圖', '以及', viki_image_count, '張圖片')
+	
+# 主要的 function
+def main():
+	lines = read_file('LINE-Viki.txt')	
+	lines = count(lines)	
+
+main()
+
+
+
+
+
